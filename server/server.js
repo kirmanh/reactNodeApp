d29d64d46247d3a211b06ca05599ea58233a56f2
kirman <hakeemkirman@gmail.com>
2023-12-28 15:56:02 +0500

    this is version 0.3

--- server/server.js
+++ server/server.js
@@ -46,13 +46,53 @@ app.use(cors());
 //   }
 // });
 
+// app.post("/register", async (req, res) => {
+//   const { userName, userEmail, userPassword } = req.body;
+
+//   const hashedPassword = await bcrypt.hash(userPassword, 10);
+
+//   const client = await pool.connect();
+//   try {
+//     await client.query("BEGIN");
+
+//     const result = await client.query(
+//       "INSERT INTO usersdata (username, email, password) VALUES ($1, $2, $3) RETURNING id",
+//       [userName, userEmail, hashedPassword]
+//     );
+
+//     await client.query("COMMIT");
+
+//     res.json({ success: true, userId: result.rows[0].id });
+//   } catch (error) {
+//     await client.query("ROLLBACK");
+//     console.error("Error during registration:", error);
+//     res.status(500).json({ success: false, error: "Internal Server Error" });
+//   } finally {
+//     client.release();
+//   }
+// });
+
 app.post("/register", async (req, res) => {
   const { userName, userEmail, userPassword } = req.body;
 
-  const hashedPassword = await bcrypt.hash(userPassword, 10);
-
+  // Check if the username or email already exists
   const client = await pool.connect();
   try {
+    const existingUser = await client.query(
+      "SELECT * FROM usersdata WHERE username = $1 OR email = $2",
+      [userName, userEmail]
+    );
+
+    if (existingUser.rows.length > 0) {
+      // User with the provided username or email already exists
+      return res
+        .status(400)
+        .json({ success: false, error: "Username or email already exists" });
+    }
+
+    // If username and email are unique, proceed with registration
+    const hashedPassword = await bcrypt.hash(userPassword, 10);
+
     await client.query("BEGIN");
 
     const result = await client.query(
@@ -73,17 +113,17 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const { userEmail, userPassword } = req.body;
+  const { userName, userPassword } = req.body;
 
   const client = await pool.connect();
   try {
     const result = await client.query(
-      "SELECT id, password FROM usersdata WHERE email = $1",
-      [userEmail]
+      "SELECT id, password FROM usersdata WHERE username = $1",
+      [userName]
     );
 
-    console.log("User Email:", userEmail);
-    console.log("User Password:", userPassword);
+    // console.log("User Email:", userEmail);
+    // console.log("User Password:", hashedPassword);
 
     if (result.rows.length === 0) {
       // User not found
@@ -103,17 +143,51 @@ app.post("/login", async (req, res) => {
     }
 
     // Generate a JWT token
-    const token = jwt.sign({ userId: result.rows[0].id }, "your-secret-key", {
+    const token = jwt.sign({ userId: result.rows[0].id }, "secret-key", {
       expiresIn: "300s",
     });
-
-    res.json({ success: true, token });
+    res.json({ success: true, token, userName });
+    console.log(token, userName);
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).json({ success: false, error: "Internal Server Error" });
   } finally {
     client.release();
   }
+});
+
+app.post("/api/articles", (req, res) => {
+  const { title, content, userId } = req.body;
+
+  pool
+    .query(
+      "INSERT INTO usersarticles (title, content, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [title, content, userId]
+    )
+    .then((result) => res.json(result.rows[0]))
+    .catch((err) => res.status(400).json("Error creating article"));
+});
+
+app.put("/api/articles/:articleId", (req, res) => {
+  const { title, content } = req.body;
+  const { articleId } = req.params;
+
+  db("usersarticles")
+    .where({ article_id: articleId })
+    .update({ title, content })
+    .returning("*")
+    .then((article) => res.json(article[0]))
+    .catch((err) => res.status(400).json("Error updating article"));
+});
+
+app.delete("/api/articles/:articleId", (req, res) => {
+  const { articleId } = req.params;
+
+  db("usersarticles")
+    .where({ article_id: articleId })
+    .del()
+    .then(() => res.json("Article deleted successfully"))
+    .catch((err) => res.status(400).json("Error deleting article"));
 });
 
 app.listen(port, () => {
